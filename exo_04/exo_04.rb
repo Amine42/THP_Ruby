f363d3f7138d4f7f3227f710198ec18e98f2e117
Amine42 <amine.bouraghda@gmail.com>
2019-01-04 07:20:40 +0100

    exo_04 FINI

---
+++ exo_04/exo_04.rb
@@ -0,0 +1,5 @@
+# Que se passe-t-il ? Pourquoi ?
+# J'obtien une erreur -> unterminated string meets end of file
+# La raison de cette erreur est le fait qu'il manque le crochet fermant ( " ) de la string
+
+puts "Salut, ça farte ?

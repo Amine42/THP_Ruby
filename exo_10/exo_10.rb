372b7908bae5d8e9f756d667ad56fac680083319
Amine42 <amine.bouraghda@gmail.com>
2019-01-04 10:14:30 +0100

    exo_10 FINI

---
+++ exo_10/exo_10.rb
@@ -0,0 +1,9 @@
+puts "Bonjour, c'est quoi ton aannee de naissance ?"
+print "> "
+user_year_birth = gets.chomp.to_i
+year = 2017
+if user_year_birth <= year
+    puts "En 2017 tu avait #{year - user_year_birth} ans"
+else
+    puts "Si tu n'es pas nee avant 2017 ce programme n'es pas fait pour toi."
+end

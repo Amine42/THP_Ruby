72eaafae301845f7a9b566de972bfeffe9f2b606
Amine42 <amine.bouraghda@gmail.com>
2019-01-04 07:07:04 +0100

    exo_2 FINI

---
+++ exo_02/exo_02.rb
@@ -0,0 +1,4 @@
+puts "Bonjour, monde !\nEt avec une voix sexy, ça donne : Bonjour, monde !"
+
+# peux-tu connaître la différence entre print et puts ?
+# puts met un retour a la ligne en plus de chaine de caracteres qu'on lui donne.

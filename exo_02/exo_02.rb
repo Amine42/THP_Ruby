5ac2509c4146ee1a6c6b0ab179f36c6b7abe7b9f
Amine42 <amine.bouraghda@gmail.com>
2019-01-04 21:35:48 +0100

    corection exo_02

--- exo_02/exo_02.rb
+++ exo_02/exo_02.rb
@@ -1,4 +1,4 @@
 puts "Bonjour, monde !\nEt avec une voix sexy, ça donne : Bonjour, monde !"
 
 # peux-tu connaître la différence entre print et puts ?
-# puts met un retour a la ligne en plus de chaine de caracteres qu'on lui donne.
+# puts met un retour a la ligne apres avoir afficher la chaine de caracteres qu'on lui donne, print affiche la string sans ajouter de retour a la ligne a la fin de la string.

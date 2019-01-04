91f1c827d9652b5bfbe575d26b971f8edb149966
Amine42 <amine.bouraghda@gmail.com>
2019-01-04 10:47:06 +0100

    exo_15 corriger

--- exo_15/exo_15.rb
+++ exo_15/exo_15.rb
@@ -2,6 +2,6 @@ puts "Bonjour, entrez votre annee de naissance"
 print "> "
 user_year_birth = gets.chomp.to_i
 age = -1
-for year in user_year_birth..2018
+for year in user_year_birth..2017
     puts "En l'an #{year} vous aviez #{age += 1}ans!"
 end

85eea5829525bc1b171b67bb19532bd4a38bb3ae
Amine42 <amine.bouraghda@gmail.com>
2019-01-04 23:13:32 +0100

    exo_07 updated

--- exo_07/exo_07_c.rb
+++ exo_07/exo_07_c.rb
@@ -2,5 +2,5 @@ user_name = gets.chomp
 puts user_name
 
 # Quelles sont les différences entre les trois programmes ?
-# La seule diference entre ces programmes c'est que certins sont plus facile a utiliser par l'utilisateur que d'autres, grace au puts qui permettent de comuniquer avec l'utilisateur et de lui demander ce qu'on attend de lui.
-# le programme B est le plus simple a utiliser suivi du A le programme C affiche ce que l'utilisateur lui envoi sans demander a l'utilisateur son blase.
+# La seule diference entre ces programmes c'est que certins sont plus facile a utiliser par l'utilisateur que d'autres, grace a l'utilisation de print et de puts qui permettent de comuniquer avec l'utilisateur et de lui demander ce qu'on attend de lui.
+# le programme B est le plus simple a utiliser suivi du A le programme, car ces deux programmes communiquent avec l'utilisateur en lui indiquant quelle valeut ils attendent de lui. Le C affiche ce que l'utilisateur lui envoi sans demander a l'utilisateur son nom quand l'utilisateur lance le programme.

27ff73238dff3283251fbd0c538ff5044826d2ab
Amine42 <amine.bouraghda@gmail.com>
2019-01-04 07:15:27 +0100

    exo_03 FINI

---
+++ exo_03/exo_03.rb
@@ -0,0 +1,5 @@
+puts "Bonjour, monde !"
+# puts "Et avec une voix sexy, ça donne : Bonjour, monde !"
+
+# Peux-tu me dire ce qu'il se passe ?
+# La ligne 2 est un commentaire, cette ligne est ignoree par l'interpreteur.
